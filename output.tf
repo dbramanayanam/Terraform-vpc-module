dea2c84af1836f44d6eed056a92b781186523758
Dinesh babu <dbramanayanam@gmail.com>
2024-04-10 19:01:30 +0400

    modified file

--- output.tf
+++ output.tf
@@ -6,17 +6,23 @@ output "vpc_id" {
   value = aws_vpc.vpc.id
 }
 
+output "internet_gw" {
+  value       = aws_internet_gateway.gw.id
 
-# output "public_subnet_ids" {
-#   value = aws_subnet.public[*].id
-# }
-
-
-# output "private_subnet_ids" {
-#   value = aws_subnet.private[*].id
-# }
+}
 
 
-# output "database_subnet_ids" {
-#   value = aws_subnet.database[*].id
-# }
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[count.index].id
+}
+
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[count.index].id
+}
+
+
+output "database_subnet_ids" {
+  value = aws_subnet.database[count.index].id
+}
