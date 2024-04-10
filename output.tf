95a4ea860ed821e62f46e8dd6812024f3e7e2eb6
Dinesh babu <dbramanayanam@gmail.com>
2024-04-10 19:55:24 +0400

    added few more files

--- output.tf
+++ output.tf
@@ -1,25 +1,3 @@
 output "azs" {
   value = local.value
 }
-
-# output "vpc_id" {
-#   value = aws_vpc.vpc.id
-# }
-
-# output "internet_gw" {
-#   value = aws_internet_gateway.gw.id
-
-# }
-
-
-# output "public_subnet_ids" {
-#   value = aws_subnet.public[*].id
-# }
-
-# output "private_subnet_ids" {
-#   value = aws_subnet.private[*].id
-# }
-
-# output "database_subnet_ids" {
-#   value = aws_subnet.database[*].id
-# }
