b728770933901692cb5fd54895ca9e72281ea2a1
Dinesh babu <dbramanayanam@gmail.com>
2024-04-10 21:39:23 +0400

    added few more files

--- output.tf
+++ output.tf
@@ -2,10 +2,15 @@ output "az_s" {
   value = local.value
 }
 
-output "vpc" {
-  value       = aws_vpc.vpc
+# output "vpc" {
+#   value       = aws_vpc.vpc
 
+# }
+
+output "aws_route_table"  {
+  value = aws_route_table.public_rt.id 
 }
+
 
 # output "database_subnets" {
 #   value       = aws_subnet.database[count.index].id
