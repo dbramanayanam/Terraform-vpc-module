34bfa00a279ad270ed6f2299d31cc563c7a68687
Dinesh babu <dbramanayanam@gmail.com>
2024-04-10 19:48:28 +0400

    added few more files

--- output.tf
+++ output.tf
@@ -12,10 +12,9 @@ output "internet_gw" {
 }
 
 
-
-# output "public_subnet_ids" {
-#   value = aws_subnet.public[*].id
-# }
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
 
 # output "private_subnet_ids" {
 #   value = aws_subnet.private[*].id
