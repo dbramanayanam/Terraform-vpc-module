9ee8fd186e1c2385fbe9bb538b9eea7518075208
Dinesh babu <dbramanayanam@gmail.com>
2024-04-10 17:36:16 +0400

    added few more files

--- output.tf
+++ output.tf
@@ -7,16 +7,16 @@ output "vpc_id" {
 }
 
 
-output "public_subnet_ids" {
-  value = aws_subnet.public[*].id
-}
+# output "public_subnet_ids" {
+#   value = aws_subnet.public[*].id
+# }
 
 
-output "private_subnet_ids" {
-  value = aws_subnet.private[*].id
-}
+# output "private_subnet_ids" {
+#   value = aws_subnet.private[*].id
+# }
 
 
-output "database_subnet_ids" {
-  value = aws_subnet.database[*].id
-}
+# output "database_subnet_ids" {
+#   value = aws_subnet.database[*].id
+# }
